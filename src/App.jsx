e75183ef0fcabc44d6b17bb81c3bc48f76059705
leomcg <leomcg@gmail.com>
2026-04-09 14:54:39 -0300

    install and create json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Main from "./MainComponent";
 
 function App() {
+  const fetchData = () => {
+    fetch("/questions").then((res) => {
+      res
+        .json()
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    });
+  };
+
+  useEffect(() => {
+    const data = fetchData();
+    console.log(data);
+  }, []);
+
   return (
     <div className="app">
       <Header />
