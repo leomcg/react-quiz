06d61ba9326a66611d9aaf2fe9546ad1d67c3869
leomcg <leomcg@gmail.com>
2026-04-09 14:20:59 -0300

    Header and Main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./MainComponent";
 
 function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
